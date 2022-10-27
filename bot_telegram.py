cad06721345de724f898a30cc9100b526bf2780d
atatarikov <atatarikov@gmail.com>
2022-10-27 14:31:36 +0300

    first commit

---
+++ bot_telegram.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+import os
+
+bot = Bot(token=os.getenv('TOKEN'))
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo_send(massage: types.Message):
+    await massage.answer(massage.text)
+    pass
+
+
+executor.start_polling(dp, skip_updates=True)
